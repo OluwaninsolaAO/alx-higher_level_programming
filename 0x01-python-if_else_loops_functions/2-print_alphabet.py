618635762daa9a6e1ae2cec1923c29616cac38f7
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-11-29 22:17:35 +0000

    Task 02

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for x in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+          'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+          's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
+    print(x, end='')
