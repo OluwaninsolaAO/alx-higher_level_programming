0b182cddeb66f174e6cd9bcc2e14e56196fe8a6d
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-11-29 23:10:01 +0000

    Task 06

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for x in range(10):
     for y in range(10):
         if x < y:
-            print("{}{}".format(x,y), end="")
+            print("{}{}".format(x, y), end="")
             if (x + 1) != 9:
                 print(", ".format(), end="")
 print()
