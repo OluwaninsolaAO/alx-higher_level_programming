5899506dec3bd993601c7d1bc827236960ce826a
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-11-29 01:35:49 +0000

    Task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,5 +8,22 @@
  */
 int check_cycle(listint_t *list)
 {
+	listint_t *slow, *fast;
+
+	if (list == NULL || list->next == NULL)
+		return(0);
+
+	slow = list->next;
+	fast = list->next->next;
+
+	while (slow && fast && fast->next)
+	{
+		if (slow == fast)
+			return (1);
+
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
 	return (0);
 }
