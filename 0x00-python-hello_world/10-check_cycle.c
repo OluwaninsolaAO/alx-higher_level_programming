7370ff3d7b28c77b83a3236a7f8467ed3a2fde18
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-11-29 01:15:08 +0000

    Task 100

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if there is a cycle/loop in a singly
+ * linked list.
+ * @list: passed list.
+ * Return: 0 on Sucess.
+ */
+int check_cycle(listint_t *list)
+{
+	return (0);
+}
--- 0x00-python-hello_world/10-linked_lists.c
+++ 0x00-python-hello_world/10-linked_lists.c
@@ -9,19 +9,18 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    const listint_t *current;
-    unsigned int n; /* number of nodes */
-
-    current = h;
-    n = 0;
-    while (current != NULL)
-    {
-        printf("%i\n", current->n);
-        current = current->next;
-        n++;
-    }
-
-    return (n);
+	const listint_t *current;
+	unsigned int n; /* number of nodes */
+	
+	current = h;
+	n = 0;
+	while (current != NULL)
+	{
+		printf("%i\n", current->n);
+		current = current->next;
+		n++;
+	}
+	return (n);
 }
 
 /**
