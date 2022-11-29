bb869fc7fbe8d73e96fd8d5c7330ca54ccbd0572
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-11-29 22:27:50 +0000

    Task 03

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for x in ['a', 'b', 'c', 'd', 'f', 'g', 'h', 'i',
+          'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r',
+          's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
+    print("{}".format(x), end='')
