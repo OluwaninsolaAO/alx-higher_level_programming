0c0e1408ae82f735d7d5e04d44064be0d3a4256e
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-11-29 21:06:33 +0000

    Task 01

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-100000, 10000)
+temp = str(number)
+num = int(temp[-1])
+str = 'Last digit of ' + temp + ' is ' + str(num)
+if num > 5:
+    str = str + ' and is greater than 5'
+elif num == 0:
+    str = str + ' and is 0'
+elif num < 6 and num != 0:
+    str = str + ' and is less than 6 and not 0'
+print(str)
