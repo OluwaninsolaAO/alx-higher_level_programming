2cda7651243d242ee42855a50205d98402379a45
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-12-01 21:03:38 +0000

    Task 01

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def add(a, b):
+    """Addition"""
+    return (a + b)
+
+
+def sub(a, b):
+    """Subtration"""
+    return (a - b)
+
+
+def mul(a, b):
+    """Multipplication"""
+    return (a * b)
+
+
+def div(a, b):
+    """Division"""
+    return (a / b)
