8990356fb80a721f47e7aa7566bc36a9636b6eee
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-12-03 21:37:49 +0000

    Task 04

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    
-    if idx < 0:
-        return my_list
-    elif idx >= len(my_list):
-        return my_list
+
+    if not my_list:
+        pass
     else:
-        dup_list = my_list[:]
-        dup_list[idx] = element
-        return dup_list
+        if idx < 0:
+            return my_list
+        elif idx >= len(my_list):
+            return my_list
+        else:
+            dup_list = my_list[:]
+            dup_list[idx] = element
+            return dup_list
