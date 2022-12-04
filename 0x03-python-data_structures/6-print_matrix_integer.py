0322c9c2e142f93b492813eae2e92d5c3207ffbf
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-12-04 07:56:28 +0000

    Task 06

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in range(len(matrix)):
         for col in range(len(matrix[row])):
+            if col != 0:
+                print(" ", end='')
             print("{:d}".format(matrix[row][col]), end="")
-            if col + 1 == len(matrix[row]):
-                print()
-            else:
-                print(end=" ")
+        print()
