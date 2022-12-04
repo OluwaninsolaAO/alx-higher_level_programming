fc71ca345922b95356d8ff167f205d8de14c142f
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-12-04 07:23:50 +0000

    Task 05

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if not my_string:
+        pass
+    else:
+        new_string = ""
+        for x in range(len(my_string)):
+            if not my_string[x] in ['c', 'C']:
+                new_string = new_string + my_string[x]
+        return new_string
