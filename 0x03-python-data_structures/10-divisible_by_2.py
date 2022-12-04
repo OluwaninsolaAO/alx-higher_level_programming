f47343b61637808c922935e41e4167611d5c88b6
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-12-04 09:11:40 +0000

    Task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    """Checks for a multiple of 2 in a list of integers"""
+
+    if not my_list:
+        return None
+    else:
+        new_list = []
+        for i in my_list:
+            if my_list[i] % 2 == 0:
+                new_list.append(True)
+            else:
+                new_list.append(False)
+        return new_list
