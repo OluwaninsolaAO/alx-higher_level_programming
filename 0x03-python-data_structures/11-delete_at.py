d155ba87d7b3e46b208a24c24b938722175c05d0
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-12-05 02:27:47 +0000

    Task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     """Delete from my_list on the index idx"""
 
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
 
     del(my_list[idx])
