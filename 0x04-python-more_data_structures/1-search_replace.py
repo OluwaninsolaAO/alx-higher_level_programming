776a205490864a473212c12fd4b0d69bad4692a5
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-12-07 22:48:47 +0000

    Task 01

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,4 +2,4 @@
 def search_replace(my_list, search, replace):
     """Replaces search with replace in my_list"""
 
-    return [list(map(lambda x: replace if x == search else x, my_list))]
+    return list(map(lambda x: replace if x == search else x, my_list))
