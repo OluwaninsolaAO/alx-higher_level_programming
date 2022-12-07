e060622a3200cc862c5f006e8347b79cbc432936
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-12-07 23:17:40 +0000

    Task 06

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@ def print_sorted_dictionary(a_dictionary):
     """Print sorted dictionary"""
 
     for x in sorted(a_dictionary):
-        print("{} : {}".format(x, a_dictionary[x]))
+        print("{}: {}".format(x, a_dictionary[x]))
