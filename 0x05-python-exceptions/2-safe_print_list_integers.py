3745dc622458f2c54291214e9425ed97f3c4cb24
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-12-19 22:16:53 +0000

    Task 02

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 def safe_print_list_integers(my_list=[], x=0):
     """Safe print all integers in a given list"""
 
-    return_value = 0;
+    return_value = 0
     for num in range(x):
         try:
             print("{:d}".format(my_list[num]), end="")
