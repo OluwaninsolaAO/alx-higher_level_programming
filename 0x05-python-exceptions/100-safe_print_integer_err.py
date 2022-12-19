10c827db7ec5b007d07d8f6f7ede2baa4ab7ce8f
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-12-19 23:06:22 +0000

    Task 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
     """Safely print integer with  error handling"""
+    import sys
     try:
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError) as err:
-        print("Exception: ", err)
+        sys.stderr.write("Exception: {}\n".format(err))
         return False
