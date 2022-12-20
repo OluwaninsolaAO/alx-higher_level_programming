fa5dd903c5a4fab9c1cc3d23c955435935275df8
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-12-20 21:55:37 +0000

    Task 03

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a class Square with  its various properties"""
+
+
+class Square:
+    """Just a square class, with all its awesome
+    fields and attributes"""
+
+    def __init__(self, size=0):
+        """Object/Instance Initial Setup"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Computes the area of a square using ``__size``"""
+        return self.__size * self.__size
