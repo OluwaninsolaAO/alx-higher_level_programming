e2b2b692efb92d964a4a62df37a19eba8eba4859
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-12-20 21:40:10 +0000

    Task 01

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a class Square with  its various properties"""
+
+
+class Square:
+    """Just a square class, with all its awesome
+    fields and attributes"""
+
+    def __init__(self, size):
+        """Object/Instance Initial Setup"""
+        self.__size = size
