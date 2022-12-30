fb0c29d050108c66920fc753483ef658a5dbdadf
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2022-12-30 06:39:36 +0000

    Task 00

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Simple addition of two numbers --
+of type int or float"""
+
+def add_integer(a, b=98):
+    """Returns a + b"""
+
+    types = [int, float]
+
+    if type(a) not in types:
+        raise TypeError("a must be an integer")
+    if type(b) not in types:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
