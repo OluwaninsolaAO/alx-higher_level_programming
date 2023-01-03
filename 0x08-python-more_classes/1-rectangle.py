453f2574284329b7d18290ddec61a3ef5f9eb93d
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-01-03 06:18:40 +0000

    Task 01

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Just a rectangle Class
+"""
+
+
+class Rectangle:
+    """
+    A rectangle class
+    """
+    def __init__(self, width=0, height=0):
+        """Initialize the class"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """getter for width"""
+        return __width
+
+    @width.setter
+    def width(self, value):
+        """setter for width"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter for height"""
+        return __height
+
+    @height.setter
+    def height(self, value):
+        """setter for height"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
