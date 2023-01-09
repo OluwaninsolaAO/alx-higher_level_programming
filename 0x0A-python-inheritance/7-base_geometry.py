8985ee4c2d20957556723666b291c0dc4eb793d4
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-01-09 14:59:51 +0000

    Task 07

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+A class named BaseGeometry -- it does stuff
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry -- does stuff
+
+    @class methods
+    area
+    integer_validator
+    """
+    def area(self):
+        """
+        Raises a warning
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates the variables ``name`` and ``values``
+        passed to the method
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value < 0:
+            raise ValueError("{} must be greater than 0".format(name))
