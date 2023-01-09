3269355259bec5523f634c45777f494882947c64
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-01-09 14:37:24 +0000

    Task 06

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A class named BaseGeometry -- it does stuff
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry -- does stuff
+
+    @class methods
+    area
+    """
+    def area(self):
+        raise Exception("area() is not implemeneted")
