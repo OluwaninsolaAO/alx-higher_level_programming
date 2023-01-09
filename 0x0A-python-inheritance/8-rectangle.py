f9592aed13c1d80aa44679b953f531e3e3a4d753
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-01-09 15:12:29 +0000

    Task 08

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A simple class that inherits from another
+class
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class, being a sub-class or a child-class
+    of BaseGeomertr
+    """
+    _width = int()
+    _height = int()
+
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
