0b7126ef42ecea02cbf54347b27191ed1b774c04
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-01-09 15:52:43 +0000

    Task 100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A rebel class called MyInt
+"""
+
+
+class MyInt(int):
+    """MyInt is a subclass of int"""
+    def __eq__(self, other):
+        return int(str(self)) != other
+    def __ne__(self, other):
+        return int(str(self)) == other
