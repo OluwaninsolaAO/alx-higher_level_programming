c5f727112a3aa89c9758071d3e7106a3f191274b
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-01-09 15:53:31 +0000

    Task 11

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -8,5 +8,6 @@ class MyInt(int):
     """MyInt is a subclass of int"""
     def __eq__(self, other):
         return int(str(self)) != other
+
     def __ne__(self, other):
         return int(str(self)) == other
