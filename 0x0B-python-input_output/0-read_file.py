68bf05076bcf808ba5e7e37318f74842c60fe50b
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-01-10 15:10:32 +0000

    Task 00

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,5 +9,9 @@ import os
 
 
 def read_file(filename=""):
+    """
+    ``read_file`` reads a content of a file
+    ``filename`` and prints it to standard output.
+    """
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
