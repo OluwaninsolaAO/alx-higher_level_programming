edc356e9769d42bc3c0e77a63e255a29e67facf3
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-01-14 12:18:22 +0100

    Task 03

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -40,6 +40,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Setter for width"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -50,6 +54,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Setter for height"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -60,6 +68,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Setter for x"""
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -70,5 +82,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Setter for y"""
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
-
--- 0x0C-python-almost_a_circle/tests/tests_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/tests_models/test_rectangle.py
@@ -32,3 +32,99 @@ class Test_Rectangle(unittest.TestCase):
         self.assertEqual(r2.width, 8)
         self.assertEqual(r2.x, 10)
         self.assertEqual(r2.y, 20)
+
+    def test_str_width(self):
+        with self.assertRaisesRegex(TypeError, 'width must be an integer'):
+            Rectangle("12", 15)
+
+    def test_None_width(self):
+        with self.assertRaisesRegex(TypeError, 'width must be an integer'):
+            Rectangle(None, 15)
+
+    def test_list_width(self):
+        with self.assertRaisesRegex(TypeError, 'width must be an integer'):
+            Rectangle([1, 2, 3], 15)
+
+    def test_float_width(self):
+        with self.assertRaisesRegex(TypeError, 'width must be an integer'):
+            Rectangle(3.1416, 15)
+
+    def test_tuple_width(self):
+        with self.assertRaisesRegex(TypeError, 'width must be an integer'):
+            Rectangle((1, 2), 15)
+
+    def test_str_height(self):
+        with self.assertRaisesRegex(TypeError, 'height must be an integer'):
+            Rectangle(12, "15")
+
+    def test_None_height(self):
+        with self.assertRaisesRegex(TypeError, 'height must be an integer'):
+            Rectangle(15, None)
+
+    def test_list_height(self):
+        with self.assertRaisesRegex(TypeError, 'height must be an integer'):
+            Rectangle(15, [1, 2, 3])
+
+    def test_float_height(self):
+        with self.assertRaisesRegex(TypeError, 'height must be an integer'):
+            Rectangle(15, 3.1416)
+
+    def test_tuple_height(self):
+        with self.assertRaisesRegex(TypeError, 'height must be an integer'):
+            Rectangle(15, (1, 2))
+
+    def test_str_x(self):
+        with self.assertRaisesRegex(TypeError, 'x must be an integer'):
+            Rectangle(10, 5, x='5')
+
+    def test_None_x(self):
+        with self.assertRaisesRegex(TypeError, 'x must be an integer'):
+            Rectangle(10, 5, x=None)
+
+    def test_tuple_x(self):
+        with self.assertRaisesRegex(TypeError, 'x must be an integer'):
+            Rectangle(10, 5, x=(10, 5))
+
+    def test_float_x(self):
+        with self.assertRaisesRegex(TypeError, 'x must be an integer'):
+            Rectangle(10, 5, x=3.1416)
+
+    def test_str_y(self):
+        with self.assertRaisesRegex(TypeError, 'y must be an integer'):
+            Rectangle(10, 5, y='5')
+
+    def test_None_y(self):
+        with self.assertRaisesRegex(TypeError, 'y must be an integer'):
+            Rectangle(10, 5, y=None)
+
+    def test_tuple_y(self):
+        with self.assertRaisesRegex(TypeError, 'y must be an integer'):
+            Rectangle(10, 5, y=(10, 5))
+
+    def test_float_y(self):
+        with self.assertRaisesRegex(TypeError, 'y must be an integer'):
+            Rectangle(10, 5, y=3.1416)
+
+    def test_neg_width(self):
+        with self.assertRaisesRegex(ValueError, 'width must be > 0'):
+            Rectangle(-5, 15)
+
+    def test_neg_height(self):
+        with self.assertRaisesRegex(ValueError, 'height must be > 0'):
+            Rectangle(5, -15)
+
+    def test_zero_width(self):
+        with self.assertRaisesRegex(ValueError, 'width must be > 0'):
+            Rectangle(0, 15)
+
+    def test_zero_height(self):
+        with self.assertRaisesRegex(ValueError, 'height must be > 0'):
+            Rectangle(5, 0)
+
+    def test_neg_x(self):
+        with self.assertRaisesRegex(ValueError, 'x must be >= 0'):
+            Rectangle(10, 5, x=-5)
+
+    def test_neg_y(self):
+        with self.assertRaisesRegex(ValueError, 'y must be >= 0'):
+            Rectangle(10, 5, y=-10)
