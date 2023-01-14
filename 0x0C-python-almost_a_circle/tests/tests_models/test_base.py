e89fc9d4b09935e204a115535a766de5553b9760
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-01-14 03:09:14 +0100

    Task 01

--- 0x0C-python-almost_a_circle/tests/tests_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/tests_models/test_base.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""
+A unit test module for testing ``models/base.py`` module.
+"""
 import unittest
-from models.base import Base
+from models.base import Base  # import Base class
+
 
 class TestBase(unittest.TestCase):
     def test_base(self):
         tmp = Base()
         self.assertEqual(tmp.id, 1)
+
 
 if __name__ == '__main__':
     unittest.main()
