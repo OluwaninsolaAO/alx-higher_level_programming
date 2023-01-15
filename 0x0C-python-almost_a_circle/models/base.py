a24993e2f8058f4cf2a86685e37fd126c2fe05ca
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-01-15 20:24:30 +0100

    Task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -34,3 +34,20 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Writes the JSON string representation of
+        ``list_objs`` to a file:
+        - ``list_objs`` is a list if instances who inherits of Base
+        - The file name will be <class name>.json
+        - Overwrite the file if it already exists.
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w", encoding="utf-8") as fi:
+            if list_objs is None:
+                fi.write("[]")
+            else:
+                list_dict = [obj.to_dictionary() for obj in list_objs]
+                fi.write(Base.to_json_string(list_dict))
