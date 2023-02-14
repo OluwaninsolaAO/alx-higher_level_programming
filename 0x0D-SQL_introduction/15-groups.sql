62df63c884d07cf53e015d2568bf46b28ad0a08e
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-02-14 19:55:05 +0100

    Task 15

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- lists the number of records with the same score in
+-- `second_table` labeled as `number`.
+
+SELECT DISTINCT `score`, COUNT(`score`) AS 'number'
+FROM `second_table`
+GROUP BY `score`;
