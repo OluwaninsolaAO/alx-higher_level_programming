5004954c3b0b7b34fd65525b08bba0ed560ed67e
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-02-14 19:04:54 +0100

    Task 07

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,13 @@
+-- inserts new row into the table `first_table` in the
+-- database with following fields and value:
+-- + `id` = 89
+-- + `name` = 'Best School'
+
+INSERT INTO `first_table`(
+	`id`,
+	`name`
+)
+VALUES (
+	89,
+	'Best School'
+);
