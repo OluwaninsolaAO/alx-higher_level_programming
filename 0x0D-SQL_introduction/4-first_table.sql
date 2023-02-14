b5a2bc3b7b3d7caf369c502809836a8bc75ab090
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-02-14 18:44:45 +0100

    Task 04

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,9 @@
+-- Creates a table called `first_table` in the current database
+-- with the following decription:
+-- + id INT
+-- + name VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS `first_table` (
+	id INT,
+	name VARCHAR(256)
+	);
