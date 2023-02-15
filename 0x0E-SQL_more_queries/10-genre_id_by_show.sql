0af076201b36a306259a76839127697d3e815fa7
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-02-15 18:14:45 +0000

    Task 10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+-- lists all shows contained in `hbtn_0d_tvshows` that have
+-- at least one genre linked.
+
+SELECT title AS title, genre_id AS genre_id
+  FROM tv_show_genres
+       INNER JOIN tv_shows
+       ON tv_shows.id = tv_show_genres.show_id
+ ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
