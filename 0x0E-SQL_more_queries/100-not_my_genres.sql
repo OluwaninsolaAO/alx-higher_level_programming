6c0a92bdd46d475d23c2e067ff67bd1d858955fa
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-02-16 00:27:16 +0100

    Task 100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,13 +3,18 @@
 
 SELECT DISTINCT tv_genres.name AS name
   FROM tv_genres
-EXCEPT
-SELECT tv_genres.name AS name
-  FROM tv_show_genres
-       INNER JOIN tv_genres
-       ON tv_show_genres.genre_id = tv_genres.id
+       INNER JOIN tv_show_genres
+       ON tv_genres.id = tv_show_genres.genre_id
 
        INNER JOIN tv_shows
        ON tv_show_genres.show_id = tv_shows.id
- WHERE tv_shows.title = 'Dexter'
- ORDER BY name ASC;
+ WHERE tv_genres.name NOT IN (
+	SELECT tv_genres.name AS name
+	  FROM tv_genres
+	       INNER JOIN tv_show_genres
+	       ON tv_genres.id = tv_show_genres.genre_id
+
+	       INNER JOIN tv_shows
+	       ON tv_show_genres.show_id = tv_shows.id
+	       WHERE tv_shows.title = 'Dexter')
+ORDER BY tv_genres.name ASC;
