dd253baf601d31c9ba06876e33502e8949d0ba5c
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-02-15 16:21:22 +0000

    Task 00

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
-/* Lists all priviledges of the MySQL users `user_0d_1` and
-`user_0d_2` on the server.*/
+-- Lists all priviledges of the MySQL users `user_0d_1` and
+-- `user_0d_2` on the server.
 
 SHOW GRANTS FOR user_0d_1@localhost;
 
