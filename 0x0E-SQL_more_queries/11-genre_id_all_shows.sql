9288e6506003bbdb3ceae1a92664a6a7a18f87a4
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-02-15 18:55:43 +0000

    Task 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,7 @@
 -- lists all shows contained in the database `hbtn_0d_tvshows`.
 
-SELECT title, 'NULL' AS genre_id
+SELECT title, IFNULL(genre_id, 'NULL') AS genre_id
   FROM tv_show_genres
-       INNER JOIN tv_shows
+       RIGHT OUTER JOIN tv_shows
        ON tv_show_genres.show_id = tv_shows.id
- ORDER BY tv_shows.title, tv_show_genres.genre_id;
+ ORDER BY title, tv_show_genres.genre_id;
