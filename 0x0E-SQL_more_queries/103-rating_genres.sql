e20ece9c5c41c95a24a4dbbd392b02c5f8703fad
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-02-16 01:15:51 +0100

    Task 103

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,12 @@
+-- lists all genres in the database `hbtn_0d_tvshows_rate`
+-- by their rating.
+
+SELECT tv_genres.name AS name, SUM(tv_show_ratings.rate) AS rating
+  FROM tv_genres
+       INNER JOIN tv_show_genres
+       ON tv_genres.id = tv_show_genres.genre_id
+
+       INNER JOIN tv_show_ratings
+       ON tv_show_genres.show_id = tv_show_ratings.show_id
+ GROUP BY name
+ ORDER BY rating DESC;
