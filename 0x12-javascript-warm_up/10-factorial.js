d576222dcdfe04e7d20dd641d733a362c9ae762f
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-13 20:01:18 +0000

    Task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+
+function factorial (num) {
+  if (!num) {
+    return (1);
+  }
+  return (num * factorial(num - 1));
+}
+
+console.log(factorial(num));
