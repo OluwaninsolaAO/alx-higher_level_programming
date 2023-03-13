42d6debaf28174bf17d1816d77510770768aeee8
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-13 18:42:05 +0000

    Task 02

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
+import { argv } from 'node:process';
 
-if (process.argv.length > 3) {
+if (argv.length > 3) {
   console.log('Arguements found');
-} else if (process.argv.length === 3) {
+} else if (argv.length === 3) {
   console.log('Arguement fount');
 } else {
   console.log('No argument');
