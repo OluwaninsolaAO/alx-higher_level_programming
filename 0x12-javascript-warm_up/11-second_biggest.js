8e69525a2191d3a823e9ea373a140de109440249
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-13 20:26:09 +0000

    Task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+let arr = process.argv.slice(2);
+arr.sort();
+arr = [... new Set(arr)];
+
+if (arr.length <= 1) {
+  console.log(0);
+} else {
+  console.log(arr[arr.length - 2]);
+}
