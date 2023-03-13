958f182914770ad5c3754646f2cab61d3ffb5575
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-13 14:37:09 +0100

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-
-let arr = process.argv.slice(2);
-arr.sort();
-arr = [...new Set(arr)];
-
-if (arr.length <= 1) {
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  console.log(arr[arr.length - 2]);
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
