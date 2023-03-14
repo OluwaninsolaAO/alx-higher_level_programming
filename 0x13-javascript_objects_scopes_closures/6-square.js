e1ad1b06b2f062a7ff5ca1dc6418714bfeea5592
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-14 17:59:03 +0100

    Task 06

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const oldSquare = require('./5-square');
+
+module.exports = class Square extends oldSquare {
+  charPrint(c) {
+    let sym = 'X';
+    let i;
+
+    if (!c || c === '') {
+      sym = c;
+    }
+    
+    for (i = 0; i < this.height; i++) {
+      console.log(sym.repeat(this.width));
+    }
+}
