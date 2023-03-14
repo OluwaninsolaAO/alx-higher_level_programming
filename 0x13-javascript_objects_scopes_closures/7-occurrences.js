b33c2466999b6392e6185f2e03b2195de478760c
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-14 17:51:31 +0000

    Task 07

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let num = 0;
+  let i;
+
+  for (i = 0; list[i]; i++) {
+    if (list[i] === searchElement) {
+      num++;
+    }
+  }
+  return (num);
+}
