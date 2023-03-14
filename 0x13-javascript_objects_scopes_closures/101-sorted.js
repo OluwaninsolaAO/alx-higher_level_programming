0a07b8f3dac1ecb9a4169e109da61fc7c424c80d
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-14 19:59:12 +0000

    Task 101

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+const obj = {};
+
+for (let key in dict) {
+  if (dict[key] in obj) {
+    obj[dict[key]].push(key);
+  } else {
+  obj[dict[key]] = [key];
+  }
+}
+
+console.log(obj);
