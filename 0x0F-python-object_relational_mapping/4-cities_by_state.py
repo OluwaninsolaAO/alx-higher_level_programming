d449708b6b0d922d9a55f4e78443064c2f857cc7
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-17 21:25:44 +0100

    Task 04

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""lists all cities from the database hbtn_0e_4_usa"""
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    _, user, passwd, db = sys.argv
+    db = MySQLdb.connect(host='localhost', port=3306, user=user,
+                         password=passwd, db=db)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
