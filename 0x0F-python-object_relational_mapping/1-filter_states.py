1ba012ea70069eb2aaf432ee90cf5ae6b60b5da7
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-17 20:06:10 +0100

    Task 01

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa"""
+
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    _, user, passwd, db = sys.argv
+    db = MySQLdb.connect(host='localhost', port=3306, user=user,
+                         password=passwd, db=db)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%'")
+    for row in cur.fetchall():
+        print(row)
