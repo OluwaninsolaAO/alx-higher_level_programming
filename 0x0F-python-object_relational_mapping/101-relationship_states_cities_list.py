a6a23e45dcb42bdfb93b9b9b1b3da550d6796dc7
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-18 15:23:59 +0100

    Task 101

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -18,7 +18,5 @@ if __name__ == '__main__':
 
     for state in session.query(State).order_by(State.id).all():
         print('{}: {}'.format(state.id, state.name))
-        for city in state.cities:
-            print('    {}: {}'.format(city.id, city.name))
 
     session.close()
