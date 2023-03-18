6c01a71544115ff3b4f9973c473665421197f0fc
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-18 15:19:36 +0100

    Task 101

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -12,11 +12,13 @@ if __name__ == '__main__':
     _, user, passwd, db = sys.argv
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(user, passwd, db))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for instance in session.query(State).order_by(State.id):
-        print(instance.id, instance.name, sep=": ")
-        for city_ins in instance.cities:
-            print("    ", end="")
-            print(city_ins.id, city_ins.name, sep=": ")
+    for state in session.query(State).order_by(State.id).all():
+        print('{}: {}'.format(state.id, state.name))
+        for city in state.cities:
+            print('    {}: {}'.format(city.id, city.name))
+
+    session.close()
