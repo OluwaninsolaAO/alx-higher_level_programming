894cfb85f984719c11df58410d5012f50a8ea53b
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-20 12:41:28 +0100

    Task 101

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -20,6 +20,6 @@ if __name__ == '__main__':
         print('{}: {}'.format(state.id, state.name))
         for city in state.cities:
             # print('\t{}: {}'.format(city.id, city.name))
-            print("\t"f"{city.id}:  {city.name}")
+            print("\t"f"{city.id}: {city.name}")
 
     session.close()
