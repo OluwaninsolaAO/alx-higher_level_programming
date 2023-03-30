533eb8acce1a68bff9806aed53034f7843d2b3f6
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-30 20:16:52 +0100

    Task 03

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept.
-curl -s -I "$1" | grep 'Allow' | cut -d' ' -f2,3,4
+curl -s -I "$1" | grep 'Allow' | cut -d':' -f2 | cut -c 2-
