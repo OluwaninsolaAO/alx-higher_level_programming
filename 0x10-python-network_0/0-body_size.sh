6a5f3cb8eb685c7a65d22cacac8ce936279948aa
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-30 19:54:58 +0100

    Task 00

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# sends a request to that URL, and displays the size
-# of the body of the response.
-
+# takes in a URL, sends a request and displays the size of the body
 curl -s "$1" | wc -c
