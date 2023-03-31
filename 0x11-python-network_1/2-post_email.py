5a66244f75776e53d3c474b145520592794d0f20
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-31 17:41:19 +0100

    Task 02

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,14 +3,15 @@
 a parameter, and displays the body of the response
 (decoded in utf-8)"""
 
-import urllib.request
-import sys
+if __name__ == "__main__":
+    import urllib.request
+    import sys
 
-url, email = sys.argv[1:]
+    url, email = sys.argv[1:]
 
-values = {'email': email}
-data = urllib.parse.urlencode(values)
-data = data.encode('ascii')
-request = urllib.request.Request(url, data)
-with urllib.request.urlopen(request) as r:
-    print(r.read().decode('utf-8'))
+    values = {'email': email}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as r:
+        print(r.read().decode('utf-8'))
