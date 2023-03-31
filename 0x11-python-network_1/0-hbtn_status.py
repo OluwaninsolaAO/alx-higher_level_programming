57edbcac4840e55765b0fcf60cc90b319643e1d5
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-31 16:20:35 +0100

    Task

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" A Python script that fetches from 
+https://alx-intranet.hbtn.io/status """
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+with urllib.request.urlopen(url) as r:
+    print('Body response:')
+    print('\t- type: {}'.format(type(r.read())))
+    print('\t- content: {}'.format(r.read()))
+    print('\t- utf8 content: {}'.format(r.read().decode('utf-8')))
