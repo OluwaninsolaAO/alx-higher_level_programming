08ae63c48e54c6b3ac0cc1a152d189465454af99
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-04-01 07:13:28 +0100

    Task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""a Python script that takes 2 arguments in order to solve
+this challenge"""
+
+if __name__ == '__main__':
+    import sys
+    import requests
+    import json
+
+    repo, owner = sys.argv[1:]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    headers = {'Accept': 'application/vnd.github+json'}
+    r = requests.get(url, headers=headers)
+    try:
+        res = json.loads(r.text)
+        for i in range(10):
+            sha = res[i]['sha']
+            name = res[i]['commit']['author']['name']
+            print('{}: {}'.format(sha, name))
+
+    except IndexError:
+        print('None')
