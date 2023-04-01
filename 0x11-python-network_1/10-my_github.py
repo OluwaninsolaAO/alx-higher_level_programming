bb557802b650223087e46f3735f5a62f012ac7bf
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-04-01 06:22:07 +0100

    Task 09

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,10 +2,15 @@
 """takes your GitHub credentials (username and password)
 and uses the GitHub API to display your id"""
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     import sys
     import requests
+    import json
     
     username, passwd = sys.argv[1:]
-    url = 'https://api.github.com/user'
-    headers = {'Accept
+    url = 'https://api.github.com/users/' + username
+    headers = {'Accept': 'application/vnd.github+json',
+               'Authorization': 'Bearer ' + passwd}
+    r = requests.get(url, headers=headers)
+    user = json.loads(r.text)
+    print(user['id'])
