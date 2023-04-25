0ce4cf6a1436924be5f3966c57dd84b928004cd3
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-04-25 21:27:33 +0100

    Task 02

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -7,5 +7,8 @@ if (process.argv.length !== 3) {
 }
 
 request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
   console.log(`code: ${response.statusCode}`);
 });
