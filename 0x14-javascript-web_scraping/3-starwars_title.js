bab9acb6afa1d661904648d3468f837ab300549a
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-04-25 21:49:16 +0100

    Task 03

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const { error } = require('console');
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  process.exit();
+}
+
+const api = 'https://swapi-api.alx-tools.com/api/films/';
+
+request(api + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
